c1de3571f04b2e59a48583a99675b288a853d213
raulereno <raulereno@gmail.com>
2023-01-23 20:36:49 -0300

    arreglando un error de ortografia, archivo cambiado lenguage.js

--- src/const/lenguage.js
+++ src/const/lenguage.js
@@ -61,7 +61,7 @@ const lenguages = {
     },
     about: {
       title: "Sobre mí",
-      text: "En la actualidad me encuentro realizando proyectos en los que implemento tecnologías como HMTL, CSS y JavaScript, también utilice librerías como React, Bootstrap y Redux. Mi mayor orgullo fue aprender Angular con Typescript en el proyecto final del bootcamp soyHenry ya que había fechas de entregas que cumplir y un nuevo framework que aprender, también conozco la implementación de NodeJS, Express, Sequelize y PostgreSQL ya que en algunos de los proyectos mencionados he necesitado un backend que me provea de información.Para sumar valor y conocimientos estoy aprendiendo Django con Python en el curso de la Universidad de Harvard, CS50's Web Programming with Python and JavaScript.",
+      text: "En la actualidad me encuentro realizando proyectos en los que implemento tecnologías como HTML, CSS y JavaScript, también utilice librerías como React, Bootstrap y Redux. Mi mayor orgullo fue aprender Angular con Typescript en el proyecto final del bootcamp soyHenry ya que había fechas de entregas que cumplir y un nuevo framework que aprender, también conozco la implementación de NodeJS, Express, Sequelize y PostgreSQL ya que en algunos de los proyectos mencionados he necesitado un backend que me provea de información.Para sumar valor y conocimientos estoy aprendiendo Django con Python en el curso de la Universidad de Harvard, CS50's Web Programming with Python and JavaScript.",
     },
     contact: {
       title: "Contacto",
